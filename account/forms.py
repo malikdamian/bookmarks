6c9cd7d806c9804a56a002bf92dd222a6677a30a
Damian Malik <malikdamiann@gmail.com>
2022-01-15 12:49:50 +0100

    Add user registration

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Hasło')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Powtórz hasło')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password_2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są identyczne.')
+        return cd['password2']
--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from account.views import user_login, dashboard
+from account.views import user_login, dashboard, register
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
-
+    path('register/', register, name='register'),
     path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
 ]
