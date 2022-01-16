842011aa0a0009588b1d6437b9b7d3ab1cf43a9e
Damian Malik <malikdamiann@gmail.com>
2022-01-16 13:10:17 +0100

    Add social authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.pl', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'social_django',
+    'django_extensions'
 
 
 ]
