d678b15e831c2e221fb9ac4f59a753ad2485a219
Damian Malik <malikdamiann@gmail.com>
2022-01-16 11:54:46 +0100

    Add authentication mechanism

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -133,3 +133,9 @@ LOGOUT_URL = 'logout'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend'
+]
