a5cd7863f92f26b2483fba4b2e5713aa33b3e5ad
Damian Malik <malikdamiann@gmail.com>
2022-01-18 19:28:35 +0100

    Add Image to admin panel

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from images.models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'created', 'image']
+    list_filter = ['created']
