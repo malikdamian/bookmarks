4390bc310e6ce36efbfb10faa101ff7fe392a294
Damian Malik <malikdamiann@gmail.com>
2022-01-24 13:29:19 +0100

    Fix decorators

--- common/decorators.py
+++ common/decorators.py
@@ -6,7 +6,6 @@ def ajax_required(func):
         if not request.is_ajax():
             return HttpResponseBadRequest()
         return func(request, *args, **kwargs)
-        wrap.__doc__ = func.__doc__
-        wrap.__name__ = func.__name__
-
+    wrap.__doc__ = func.__doc__
+    wrap.__name__ = func.__name__
     return wrap
