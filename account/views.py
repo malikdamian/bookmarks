68774f705e04ca5f09371dc26d592c8edd1df954
Damian Malik <malikdamiann@gmail.com>
2022-01-31 14:42:53 +0100

    Optimizing QuerySets dashboard method

--- account/views.py
+++ account/views.py
@@ -19,7 +19,8 @@ def dashboard(request):
                                                        flat=True)
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')\
+        .prefetch_related('target')[:10]
 
     return render(request,
                   'account/dashboard.html',
